ad5d5a9e50e5c745514ac6ca4825c3531d94efb2
Asmae <36376292+asmafatiha@users.noreply.github.com>
2024-02-08 16:28:35 +0100

    Update PersonService.java

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -44,7 +44,7 @@ public class PersonService {
 		p.setId("5");
 		p.setAge(25);
 		p.setFirstName("webhook");
-		p.setLastName("testJenjinsJob");
+		p.setLastName("gggggggggggggggggggggggggggggggggggggggggggggggggg");
 		persons.add(p);
 		
 	}
